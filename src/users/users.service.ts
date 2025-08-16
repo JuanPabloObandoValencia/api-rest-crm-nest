d910e6d48ef36b24e0f0528de7d366445fbff35f
JuanPabloObandoValencia <juan.obando@gruponex.com.co>
2025-08-15 19:49:47 -0500

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+
+    return this.prisma.user.create({
+
+      data: {
+
+        email: createUserDto.email,
+        name: createUserDto.name,
+        password: createUserDto.password,
+
+      }
+
+    })
+
+  }
+
+  async findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
